bbf020e50108e60a57bc55931495e87d4624b408
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2021-08-17 21:53:17 +0200

    fixup! add value crate

--- crates/value/src/lib.rs
+++ crates/value/src/lib.rs
@@ -1,16 +1,10 @@
-#[derive(Clone, Debug)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum Value {
     Nil,
     Number(f64),
     String(String),
     BareWord(String),
     Array(Vec<Value>),
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
 }
