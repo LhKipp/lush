778a06d547318f9668ce1d4fc88c23a0c1d81eb0
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2021-08-17 21:54:24 +0200

    use fsErr in lu_fs

--- crates/lu_fs/src/lib.rs
+++ crates/lu_fs/src/lib.rs
@@ -1,28 +1,14 @@
-use lu_error::{FsErr, LuErr, LuResult};
+use lu_error::{FsErr, LuResult};
 use std::{fs, path::Path};
 
 pub fn read_to_string<P: AsRef<Path>>(path: P) -> LuResult<String> {
-    fs::read_to_string(path.as_ref()).map_err(|e| {
-        LuErr::FS(FsErr {
-            path: path.as_ref().into(),
-            source: e.into(),
-        })
-    })
+    fs::read_to_string(path.as_ref())
+        .map_err(|e| FsErr::ReadFailed(path.as_ref().into(), e.to_string()))
+        .map_err(|e| e.into())
 }
 
 pub fn write<P: AsRef<Path>, C: AsRef<[u8]>>(path: P, contents: C) -> LuResult<()> {
-    fs::write(path.as_ref(), contents).map_err(|e| {
-        LuErr::FS(FsErr {
-            path: path.as_ref().into(),
-            source: e.into(),
-        })
-    })
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+    fs::write(path.as_ref(), contents)
+        .map_err(|e| FsErr::WriteFailed(path.as_ref().into(), e.to_string()))
+        .map_err(|e| e.into())
 }
