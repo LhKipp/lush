8b345503ec5755a5b71becbded67df32dd1af587
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2021-08-18 16:12:16 +0200

    move unittest to fil

--- crates/lu_interpreter/src/evaluate/cmd_call.rs
+++ crates/lu_interpreter/src/evaluate/cmd_call.rs
@@ -41,3 +41,20 @@ impl Evaluable for CmdStmtNode {
         result
     }
 }
+
+#[cfg(test)]
+mod test {
+    use lu_error::LuResult;
+    use lu_test_support::{init_logger, make_test_interpreter};
+    use lu_text_util::SourceCode;
+    use lu_value::Value;
+    use {conformance, serde_json};
+
+    #[conformance::tests(exact, serde=serde_json, file="test_data/evaluate/cmd_call/general.json_test")]
+    fn general_interpreter_tests(s: &str) -> LuResult<Value> {
+        init_logger();
+        let mut itprt = make_test_interpreter();
+
+        itprt.evaluate(SourceCode::Text(s.to_string()))
+    }
+}
