0dc41d25a964489bec287d6a776733c14c007167
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2021-08-19 10:45:22 +0200

    add test_print cmd

--- crates/lu_cmds/src/lib.rs
+++ crates/lu_cmds/src/lib.rs
@@ -1,6 +1,8 @@
 mod print;
+mod test_print;
 
 pub use print::PrintCmd;
+pub use test_print::TestPrintCmd;
 
 #[cfg(test)]
 mod tests {
---
+++ crates/lu_cmds/src/test_print.rs
@@ -0,0 +1,29 @@
+// use log::debug;
+use lu_error::LuResult;
+use lu_interpreter::{Command, Interpreter};
+use lu_value::Value;
+
+#[derive(Debug)]
+pub struct TestPrintCmd {}
+
+impl Command for TestPrintCmd {
+    fn name(&self) -> &str {
+        "tprint"
+    }
+
+    fn run(&self, state: &mut Interpreter) -> LuResult<Value> {
+        let args = match state.scope.lock().cur_frame().get_var("args").unwrap() {
+            Value::Array(vals) => vals[1..].to_vec(), // Always erase $arg.0 (cmd name)
+            _ => unreachable!(),
+        };
+        let mut scope = state.scope.lock();
+        let global_f = scope.global_mut_frame();
+        if let Some(test_print_vars) = global_f.get_mut_var("t_printed") {
+            let vals = test_print_vars.expect_array();
+            vals.extend(args)
+        } else {
+            global_f.insert_var("t_printed".to_string(), Value::new_array(args));
+        }
+        Ok(Value::Nil)
+    }
+}
