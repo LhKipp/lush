e0de8f8ae14ac5a8c90f057d3dee1d799763b041
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2021-08-20 15:48:28 +0200

    add parsing of let

--- crates/lu_parser/src/grammar/let_stmt.rs
+++ crates/lu_parser/src/grammar/let_stmt.rs
@@ -1,6 +1,8 @@
 use crate::{
+    grammar::ValueExprRule,
     parser::{CompletedMarker, Parser, CMT_NL_WS},
     SyntaxKind::*,
+    T,
 };
 
 use super::Rule;
@@ -15,7 +17,33 @@ impl Rule for LetStmtRule {
         p.next_non(CMT_NL_WS) == LetKeyword
     }
 
-    fn parse_rule(&self, _p: &mut Parser) -> Option<CompletedMarker> {
-        todo!();
+    fn parse_rule(&self, p: &mut Parser) -> Option<CompletedMarker> {
+        let m = p.start();
+        p.eat_while(CMT_NL_WS);
+        if !p.expect(LetKeyword) {
+            // We better stop here, before doing more
+            return None;
+        }
+        p.eat_while(CMT_NL_WS);
+        p.expect(BareWord);
+        p.eat_while(CMT_NL_WS);
+        p.expect(T![=]);
+        ValueExprRule {}.parse(p);
+        Some(m.complete(p, LetStmt))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{parse_as, Event};
+
+    use super::LetStmtRule;
+
+    use {conformance, serde_yaml};
+
+    #[conformance::tests(exact, serde=serde_yaml, file="test_data/grammar/let_stmt/let_simple.yaml_test")]
+    fn parse_cmds(s: &str) -> Vec<Event> {
+        lu_test_support::init_logger();
+        parse_as(s, &LetStmtRule {})
     }
 }
