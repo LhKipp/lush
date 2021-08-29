5dc803109bbade3a1256ad5e3caf7013edf2a7ee
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2021-08-29 15:31:54 +0200

    remove unused test

--- crates/lu_cmds/src/lib.rs
+++ crates/lu_cmds/src/lib.rs
@@ -3,11 +3,3 @@ mod test_print;
 
 pub use print::PrintCmd;
 pub use test_print::TestPrintCmd;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
