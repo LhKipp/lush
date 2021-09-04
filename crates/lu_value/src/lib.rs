e5d1dc4b601714be8548fd1271325b6cd7b1a4df
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2021-09-04 09:33:42 +0200

    move ValueType to own mod

--- crates/lu_value/src/lib.rs
+++ crates/lu_value/src/lib.rs
@@ -3,20 +3,8 @@ use std::{any::Any, fmt::Display, rc::Rc};
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq)]
-pub enum ValueType {
-    Unspecified,
-    Any,
-    Nil,
-    Bool,
-    Number,
-    String,
-    BareWord,
-    Array(Box<ValueType>),
-    Function,
-    /// Type with name string. Could not yet been deduced
-    Unresolved(String)
-}
+mod value_type;
+pub use value_type::ValueType;
 
 pub const NIL_VAL: Value = Value::Nil;
 
