74375180a07acdcbfcebcea90042eb7025ea1662
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2021-10-25 10:40:32 +0200

    check DBG_SESSION var value

--- crates/lu_interpreter_structs/src/special_scope_vars.rs
+++ crates/lu_interpreter_structs/src/special_scope_vars.rs
@@ -1,14 +1,23 @@
+use log::debug;
 use lu_error::lu_source_code_item;
 use lu_error::SourceCodeItem;
 
+use crate::Value;
 use crate::{Scope, ScopeFrame, Variable};
 
 const DBG_SESSION_VAR_NAME: &str = "DBG_SESSION";
 pub fn is_dbg_session(scope: &Scope<Variable>) -> bool {
-    scope.find_var(DBG_SESSION_VAR_NAME).is_some()
+    scope
+        .find_var(DBG_SESSION_VAR_NAME)
+        .map(|var| match var.val {
+            Value::Bool(v) => v,
+            _ => false,
+        })
+        .unwrap_or(false)
 }
 
 pub fn set_dbg_session(frame: &mut ScopeFrame<Variable>) {
+    debug!("Inserting DBG_SESSION=true");
     frame.insert_var(Variable::new(
         DBG_SESSION_VAR_NAME.to_string(),
         true.into(),
