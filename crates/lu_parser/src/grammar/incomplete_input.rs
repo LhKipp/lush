b6dd8863f652d399ffe64cd761607a0678b5f10d
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2021-11-09 12:59:42 +0100

    change broken input test

--- crates/lu_parser/src/grammar/incomplete_input.rs
+++ crates/lu_parser/src/grammar/incomplete_input.rs
@@ -10,9 +10,20 @@ mod tests {
         parse_as(s, &SourceFileRule {})
     }
 
-    #[lu_conformance::tests(exact, serde=serde_yaml, file="test_data/grammar/broken_input")]
-    fn parse_broken_input(s: &str) -> Vec<Event> {
-        lu_test_support::init_logger();
-        parse_as(s, &SourceFileRule {})
+    #[test]
+    fn parse_broken_input() {
+        let s = r"
+                fn asf {} ((
+                impl Rule for BlockStmtRule {
+                    fn matches(&self, p: &mut Parser) -> bool {
+                        assert!(self.parse_begin);
+                        p.next_non(CMT_NL_WS) == BeginKeyword
+                    }
+
+                let x = 1
+            ";
+        // must return as usual
+        parse_as(s, &SourceFileRule {});
+        assert!(true);
     }
 }
