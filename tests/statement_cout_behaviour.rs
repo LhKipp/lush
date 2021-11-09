0e67ca3ecfe26b8f4cc6bd88a103b226025b945e
Leonhard Kipp <Leonhard.Kipp@alumni.fh-aachen.de>
2021-11-09 15:12:08 +0100

    add test and doc for cout behaviour

--- tests/statement_cout_behaviour.rs
+++ tests/statement_cout_behaviour.rs
@@ -9,3 +9,39 @@ fn piped_cmds_stmt_do_not_print() {
     assert_eq!(cerr, "");
     assert_eq!(cout, "hi\n");
 }
+
+#[test]
+fn internal_fn_always_writes_to_stdout() {
+    let tmp_file_path = make_tmp_file(
+        r#"
+        fn print_and_ret_num
+            echo printed
+            ret 1
+        end
+        print_and_ret_num | cat
+
+        let x = print_and_ret_num
+        "#
+        .as_bytes(),
+    );
+    let (code, cout, cerr) =
+        binary::run_binary(&[&tmp_file_path.path().to_string_lossy().to_string()]);
+    assert_eq!(code, 0);
+    assert_eq!(cerr, "");
+    assert_eq!(cout, "printed\n1\nprinted\n");
+}
+
+#[test]
+fn external_cmd_not_printing_when_is_rhs_of_let() {
+    let tmp_file_path = make_tmp_file(
+        r#"
+        let x = echo hi
+        "#
+        .as_bytes(),
+    );
+    let (code, cout, cerr) =
+        binary::run_binary(&[&tmp_file_path.path().to_string_lossy().to_string()]);
+    assert_eq!(code, 0);
+    assert_eq!(cerr, "");
+    assert_eq!(cout, "");
+}
